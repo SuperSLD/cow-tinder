6cc108ae54e0e382649c0b62660d2962a1b91a4a
SuperSLD <37121925+SuperSLD@users.noreply.github.com>
2024-11-23 22:08:55 +0300

    create home screen

--- app-ui/src/main/java/com/example/app_ui/screens/auth/login/LoginPresenter.kt
+++ app-ui/src/main/java/com/example/app_ui/screens/auth/login/LoginPresenter.kt
@@ -5,6 +5,7 @@ import com.example.app_domain.usecases.user.LoginUseCase
 import com.example.app_ui.common.core.base.launchUI
 import com.example.app_ui.common.core.base.withIO
 import com.example.app_ui.ext.createHandler
+import com.example.app_ui.screens.home.HomeScreen
 import com.example.app_ui.screens.main.FlowMainScreen
 import online.jutter.supersld.common.base.BasePresenter
 import org.koin.core.inject
@@ -20,11 +21,11 @@ class LoginPresenter : BasePresenter<LoginView>() {
             viewState.toggleLoading(false)
         }) {
             viewState.toggleLoading(true)
-            router?.navigateTo(FlowMainScreen())
-            val authResponse = withIO { loginUseCase(login, password) }
-            if (!authResponse.token.isNullOrEmpty()) {
-                router?.navigateTo(FlowMainScreen())
-            }
+            router?.navigateTo(HomeScreen())
+//            val authResponse = withIO { loginUseCase(login, password) }
+//            if (!authResponse.token.isNullOrEmpty()) {
+//                router?.navigateTo(HomeScreen())
+//            }
         }
     }
 
---
+++ app-ui/src/main/java/com/example/app_ui/screens/home/HomeView.kt
@@ -0,0 +1,10 @@
+package com.example.app_ui.screens.home
+
+import com.arellomobile.mvp.MvpView
+import com.arellomobile.mvp.viewstate.strategy.AddToEndSingleStrategy
+import com.arellomobile.mvp.viewstate.strategy.SkipStrategy
+import com.arellomobile.mvp.viewstate.strategy.StateStrategyType
+
+interface HomeView : MvpView {
+
+}
